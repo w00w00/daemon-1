37c01b4aa164bf996605c714a1cf625014b5e2a6
SolomoN <solomon@solomon.if.ua>
2012-01-02 22:42:35 +0200

    Added default header comment to index.php Added version file

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+/* Project Daemon
+ *
+ * name: index.php
+ * version: 0.0.001
+ * info: main system file
+ *
+ * Copyright (c) 2012, Anatoliy Syernyi aka SolomoN
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+echo 'Project Daemon';
 
 ?>
